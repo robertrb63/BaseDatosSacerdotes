7359a2ff968a7ae63876d902e4349b2d365c5263
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2023-10-31 12:40:23 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/1.jpg'
 import viteLogo from '/2.jpeg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-    
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Roberto Antonio Restrepo Builes</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button>un nuevo boton</button>
+
         <section id="seleccionar-mascota">
       <h1 className="titulo">MOKEPON! 🔥 🌊 🌴</h1>
       <h2 className="subitulo">Eligue tu mascota:</h2>
@@ -48,7 +35,6 @@ function App() {
         <input type="radio" name="mascota" id="ratigueya" />
       </div>
 
-      <button id="boton-mascota">Seleccionar</button>
     </section>
 
     <section id="seleccionar-ataque">
@@ -65,28 +51,11 @@ function App() {
         <span id="vidas-enemigo">3</span> vidas
       </p>
 
-      <p id="seleccionar-mascota">
-        <button id="boton-fuego" className='boton-de-ataque'>Fuego 🔥</button>
-        <button id="boton-agua" className='boton-de-ataque'>Agua 🌊</button>
-        <button id="boton-tierra" className='boton-de-ataque'>Tierra 🌴</button>
-      </p>
     </section>
-
     <section id="mensajes">
-    
-      <p></p>
     </section>
 
-    <section id="reiniciar">
-      <button id="boton-reniciar">Reiniciar Juego</button>
-    </section>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        ESto es un ensayo para desplegar algo personal con vite
-      </p>
     </>
   )
 }
